e5df874c045f36e80f5012962ecc397fff7e8392
S <shakibmohammad18@gmail.com>
2024-08-08 17:23:49 +0600

    gen-imi ai chat bot

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 import 'package:gemini_ai_app/screen/genimi_ai.dart';
 
 void main(){
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner:false,
       home: GeminiChatBot(),
     );
